01e72666541dd828635af109525fde4f1b3fef99
SylvainEstebe <sylvain.estebe@gmail.com>
2023-12-30 22:17:20 +0100

    update

--- dash_deploy.py
+++ dash_deploy.py
@@ -21,11 +21,11 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div(
     [
-        html.H4("Embedding of Cognitive Distortion"),
+        html.H4("Embedding of cognitive distortion"),
         html.P("Select a model:"),
         dcc.RadioItems(
             id="selection",
-            options=["K-mean 1","K-mean 2","K-mean 3","Agglomerative 1","Agglomerative 2","Agglomerative 3","HDBSCAN 1","HDBSCAN 2","HDBSCAN 3"],
+            options=["K-mean 1","K-mean 2","K-mean 3","HDBSCAN 1 (Bayesian optimization)","HDBSCAN 2 (Bayesian optimization)","HDBSCAN 3 (Bayesian optimization)","HDBSCAN 1 (manual)","HDBSCAN 2 (manual)","HDBSCAN 3 (manual)"],
             value="K-mean 1",
         ),
         dcc.Loading(dcc.Graph(id="graph"), type="cube"),
@@ -45,7 +45,7 @@ def display_animated_graph(selection):
         y=df[f"{model} y"],
         color=  df[f"{model} k-mean"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model} model'
+        title= f'Cluster of cognitive distortion using {model} model'
         ),
         "K-mean 2": px.scatter(
         df,
@@ -53,7 +53,7 @@ def display_animated_graph(selection):
         y=df[f"{model_2} y"],
         color=  df[f"{model_2} k-mean"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_2} model'
+        title= f'Cluster of cognitive distortion using {model_2} model'
         ),
         "K-mean 3": px.scatter(
         df,
@@ -61,55 +61,55 @@ def display_animated_graph(selection):
         y=df[f"{model_3} y"],
         color=  df[f"{model_3} k-mean"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_3} model'
+        title= f'Cluster of cognitive distortion using {model_3} model'
         ),
-        "Agglomerative 1": px.scatter(
+        "HDBSCAN 1 (Bayesian optimization)": px.scatter(
         df2,
         x=df2[f"{model} x"],
         y=df2[f"{model} y"],
-        color=  df2[f"{model} aglo"].astype(str),
+        color=  df2[f"{model} hdbscan_bayesian_optimisation"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model} model'
+        title= f'Cluster of cognitive distortion using {model} model'
         ),
-        "Agglomerative 2": px.scatter(
+        "HDBSCAN 2 (Bayesian optimization)": px.scatter(
         df2,
         x=df2[f"{model_2} x"],
         y=df2[f"{model_2} y"],
-        color=  df2[f"{model_2} aglo"].astype(str),
+        color=  df2[f"{model_2} hdbscan_bayesian_optimisation"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_2} model'
+        title= f'Cluster of cognitive distortion using {model_2} model'
         ),
-        "Agglomerative 3": px.scatter(
+        "HDBSCAN 3 (Bayesian optimization)": px.scatter(
         df2,
         x=df2[f"{model_3} x"],
         y=df2[f"{model_3} y"],
-        color=  df2[f"{model_3} aglo"].astype(str),
+        color=  df2[f"{model_3} hdbscan_bayesian_optimisation"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_3} model'
+        title= f'Cluster of cognitive distortion using {model_3} model'
         ),
-        "HDBSCAN 1": px.scatter(
+        "HDBSCAN 1 (manual)": px.scatter(
         df3,
         x=df3[f"{model} x"],
         y=df3[f"{model} y"],
         color=  df3[f"{model} hdbscan_manual"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model} model'
+        title= f'Cluster of cognitive distortion using {model} model'
         ),
-        "HDBSCAN 2": px.scatter(
+        "HDBSCAN 2 (manual)": px.scatter(
         df3,
         x=df3[f"{model_2} x"],
         y=df3[f"{model_2} y"],
         color=  df3[f"{model_2} hdbscan_manual"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_2} model'
+        title= f'Cluster of cognitive distortion using {model_2} model'
         ),
-        "HDBSCAN 3": px.scatter(
+        "HDBSCAN 3 (manual)": px.scatter(
         df3,
         x=df3[f"{model_3} x"],
         y=df3[f"{model_3} y"],
         color=  df3[f"{model_3} hdbscan_manual"].astype(str),
         hover_data= 'thought',
-        title= f'Cluster of negative thought using {model_3} model'
+        title= f'Cluster of cognitive distortion using {model_3} model'
         )
     }
     return animations[selection]
